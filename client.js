57681da8178ae710d3a362e8bef6a9f698c31b79
isavega <igvega@uc.cl>
2020-05-18 21:31:10 -0400

    Orden app

--- client.js
+++ client.js
@@ -4,70 +4,13 @@ const socket = io('wss://le-18262636.bitzonte.com', {
     path: '/stocks'
 });
 
-// EVENTOS
-
-// Emitidos y recibidos por el servidor
+// Variables Globales
 
 const companies_names = [];
 const valor_acciones_tiempo = [];
 const companies_dict = {};
 const buyDict = {"AAPL": [], "FB": [], "SNAP": [], "IBM": [], "TWTR": []};
 let todos = [];
-
-socket.emit('STOCKS');
-socket.on('STOCKS', (data) => {    
-    // for (var i in data) {
-    //   companies_names.push(data[i].company_name)
-    // }
-    for (var i = 0; i < data.length; i++) {
-      companies_dict[data[i].ticker] = [];
-      var grafico = document.createElement('div');
-      console.log(`${data[i].ticker}` );
-      grafico.setAttribute("id", `${data[i].ticker}`);
-      acciones.appendChild(grafico);
-      }
-
-    socket.on('UPDATE', (data) => {
-      ticker = document.getElementById('ticker');
-      ticker.innerHTML = data.ticker;
-      value = document.getElementById('value');
-      value.innerHTML = data.value;
-      time = document.getElementById('time');
-      time.innerHTML = data.time;
-
-      companies_dict[data.ticker].push([data.time, data.value]);
-      companies_dict [data.ticker].push([data.time, data.value]);
-
-    
-      for (var i in companies_dict ) {
-        if(data.ticker == i){
-          var nombre_grafico = `${data.ticker}`;
-          drawChart(companies_dict [data.ticker], i, nombre_grafico);
-          todos.push({
-            "key": data.ticker,
-            "value":  data.value,
-          });
-         
-        };
-      };
-
-      console.log("TODOOS",todos);
-      getLastPrice(todos);
-      
-    } );
-
-    socket.on('BUY', (data) => {
-      buyDict[data.ticker].push(data.volume);
-      if(data.ticker == "FB"){
-        const obj2 = [{
-          "key": data.ticker,
-          "value":  data.volume,
-        }];
-        getVolume(obj2)
-      };
-    } );
-
-});
 
 
 // Funciones
@@ -84,6 +27,49 @@ function ManualSocketConnect() {
   console.log("Socket open ");
 }
 
+function getUpdate() {
+  socket.on('UPDATE', (data) => {
+    ticker = document.getElementById('ticker');
+    ticker.innerHTML = data.ticker;
+    value = document.getElementById('value');
+    value.innerHTML = data.value;
+    time = document.getElementById('time');
+    time.innerHTML = data.time;
+
+    companies_dict[data.ticker].push([data.time, data.value]);
+    companies_dict [data.ticker].push([data.time, data.value]);
+
+  
+    for (var i in companies_dict ) {
+      if(data.ticker == i){
+        var nombre_grafico = `${data.ticker}`;
+        drawChart(companies_dict [data.ticker], i, nombre_grafico);
+        todos.push({
+          "key": data.ticker,
+          "value":  data.value,
+        });
+       
+      };
+    };
+
+    console.log("TODOOS",todos);
+    getLastPrice(todos);
+    
+  } );
+}
+
+function getBuy() {
+  socket.on('BUY', (data) => {
+    buyDict[data.ticker].push(data.volume);
+    if(data.ticker == "FB"){
+      const obj2 = [{
+        "key": data.ticker,
+        "value":  data.volume,
+      }];
+      getVolume(obj2)
+    };
+  } );
+}
 
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
@@ -120,6 +106,8 @@ function getVolume(diccionario) {
 }
 
 
+// Google Charts 
+
 google.charts.load('current', {'packages':['line']});
       //google.charts.setOnLoadCallback(drawChart);
 
@@ -148,4 +136,33 @@ google.charts.load('current', {'packages':['line']});
       chart.draw(data, google.charts.Line.convertOptions(options));
     }
 
+
+// MENU
+
+
+function runSocket() {
+
+  socket.emit('STOCKS');
+  socket.on('STOCKS', (data) => {    
+      // for (var i in data) {
+      //   companies_names.push(data[i].company_name)
+      // }
+      for (var i = 0; i < data.length; i++) {
+        companies_dict[data[i].ticker] = [];
+        var grafico = document.createElement('div');
+        console.log(`${data[i].ticker}` );
+        grafico.setAttribute("id", `${data[i].ticker}`);
+        acciones.appendChild(grafico);
+        }
+  
+      getUpdate();    
+      getBuy();
+  
+  });
+
+}
+
+// PROGRAMA
+
+runSocket();
 
